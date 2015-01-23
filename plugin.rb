58668c814b9ce247ff713a1dcc8a971f3d4b82a5
Ryan Fox <ryan.fox@upverter.com>
2015-01-22 20:27:43 -0500

    Wrap the existing can_see_topic? and then do stuff at the end. Still playing.

--- plugin.rb
+++ plugin.rb
@@ -6,23 +6,12 @@
 after_initialize do
 
   TopicGuardian.class_eval do
+    alias_method :orig_can_see_topic?, :can_see_topic?
+
     def can_see_topic?(topic)
-      return false unless topic
-      # Admins can see everything
-      return true if is_admin?
-      # Deleted topics
-      return false if topic.deleted_at && !can_see_deleted_topics?
-
-      raise "this is me testing!"
-
-      if topic.private_message?
-        return authenticated? &&
-          topic.all_allowed_users.where(id: @user.id).exists?
-      end
-
-      # not secure, or I can see it
-      !topic.read_restricted_category? || can_see_category?(topic.category)
-
+      !orig_can_see_topic?(topic)
     end
+
+  end
 
 end
